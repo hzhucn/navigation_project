100b24659fa28683bbca619dc5580f478cc127ab
Jardel Santos <jardelsantos44@gmail.com>
2019-02-03 19:49:55 -0200

    refactor: Including Double Q-learning algorithm
    The agent was updated to add the DQL method on the DQN, modifying the dqn_agent.py file.
    
    The new agent was trained, obtaining little better results.
    
    Need to update README documentation.

--- dqn_agent.py
+++ dqn_agent.py
@@ -86,8 +86,10 @@ class Agent():
         """
         states, actions, rewards, next_states, dones = experiences
         
-        # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Get max predicted Q values (for next states) from target model - USING DOUBLE QNET IMPLEMENTATION
+        max_Q = self.qnetwork_local(next_states).detach().max(1)[1]
+        Q_targets_next = torch.tensor([self.qnetwork_target(next_states).detach()[i,max_Q[i]] for i in range(BATCH_SIZE)]).unsqueeze(1)
+
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
